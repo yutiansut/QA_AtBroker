d30f44fcab831033b4172ff815654b694fbf15c7
yutiansut <yutiansut@qq.com>
2018-11-19 02:16:08 +0800

    #update'

--- QA_AtBroker.py
+++ QA_AtBroker.py
@@ -80,6 +80,9 @@ class QA_ATBroker(QA_Broker):
         self.t.OnRtnInstrumentStatus = self.OnRtnInstrumentStatus
         self.t.OnRspOrderInsert = self.OnRspOrderInsert
         self.t.OnRtnOrder = self.OnRtnOrder
+        self.t.OnRspQryInvestorPosition = self.OnRspQryInvestorPosition
+        self.t.OnRspQryTradingAccount = self.OnRspQryTradingAccount
+        self.t.OnRspQryInvestor = self.OnRspQryInvestor
 
         self.q.OnFrontConnected = self.q_OnFrontConnected
         self.q.OnRspUserLogin = self.q_OnRspUserLogin
@@ -155,16 +158,169 @@ class QA_ATBroker(QA_Broker):
     def OnRtnInstrumentStatus(self, pInstrumentStatus: ctp.CThostFtdcInstrumentStatusField):
         # QA.QA_util_log_info('instrumentStatus')
         # QA.QA_util_log_info(str(pInstrumentStatus))
-        self._trading_code.append(str(pInstrumentStatus.InstrumentID,encoding='utf-8'))
+        """
+        SHFE: 上期所返回的是rb
+        其他交易所返回的是具体合约
+        """
+
+        self._trading_code.append(
+            str(pInstrumentStatus.InstrumentID, encoding='utf-8'))
 
     def OnRspOrderInsert(self, pInputOrder: ctp.CThostFtdcInputOrderField, pRspInfo: ctp.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
+
+        QA.QA_util_log_info('ON_RSP_Order_INSERT')
+
         QA.QA_util_log_info(pRspInfo)
         QA.QA_util_log_info(pInputOrder)
         QA.QA_util_log_info(pRspInfo.getErrorMsg())
+        QA.QA_util_log_info(bIsLast)
+
+    def OnRspQryInvestorPosition(self, pInvestorPosition: ctp.CThostFtdcInvestorPositionField, pRspInfo: ctp.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
+        """[summary]
+        
+        Arguments:
+            pInvestorPosition {ctp.CThostFtdcInvestorPositionField} -- [description]
+            pRspInfo {ctp.CThostFtdcRspInfoField} -- [description]
+            nRequestID {int} -- [description]
+            bIsLast {bool} -- [description]
+        
+        
+        InstrumentID = 'cs1909',合约代码
+        BrokerID = '9999', 经纪公司代码
+        InvestorID = '106184', 投资者代码
+        PosiDirection = PosiDirectionType.Short, 持仓多空方向
+        HedgeFlag = HedgeFlagType.Speculation, 投机套保标志
+        PositionDate = PositionDateType.Today, 持仓日期
+        YdPosition = 1, 上日持仓
+        Position = 1, 今日持仓
+        LongFrozen = 0, 多头冻结
+        ShortFrozen = 0, 空头冻结
+        LongFrozenAmount = 0.0,开多仓冻结金额
+        ShortFrozenAmount = 0.0, 开空仓冻结金额 
+        OpenVolume = 0, 开仓量
+        CloseVolume = 0, 平仓量
+        OpenAmount = 0.0,  开仓金额
+        CloseAmount = 0.0, 平仓金额
+        PositionCost = 24870.0, 持仓成本
+        PreMargin = 0.0,上次占用的保证金
+        UseMargin = 1243.5, 占用的保证金
+        FrozenMargin = 0.0,冻结的保证金
+        FrozenCash = 0.0, 冻结的资金
+        FrozenCommission = 0.0,冻结的手续费
+        CashIn = 0.0, 资金差额
+        Commission = 0.0,手续费
+        CloseProfit = 0.0, 平仓盈亏
+        PositionProfit = 30.0,持仓盈亏
+        PreSettlementPrice = 2487.0, 上次结算价
+        SettlementPrice = 2484.0,本次结算价
+        TradingDay = '20181116', 交易日
+        SettlementID = 1,结算编号
+        OpenCost = 25080.0, 开仓成本
+        ExchangeMargin = 1243.5,  交易所保证金
+        CombPosition = 0,组合成交形成的持仓
+        CombLongFrozen = 0,组合多头冻结
+        CombShortFrozen = 0, 组合空头冻结
+        CloseProfitByDate = 0.0,逐日盯市平仓盈亏
+        CloseProfitByTrade = 0.0, 逐笔对冲平仓盈亏
+        TodayPosition = 0,  今日持仓
+        MarginRateByMoney = 0.0, 保证金率
+        MarginRateByVolume = 0.0,  保证金率(按手数)
+        StrikeFrozen = 0,  执行冻结
+        StrikeFrozenAmount = 0.0, 执行冻结金额
+        AbandonFrozen = 0, 放弃执行冻结
+        ExchangeID = '',  交易所代码
+        YdStrikeFrozen = 0, 执行冻结的昨仓
+        InvestUnitID = ''投资单元代码
+
+        """
+        QA.QA_util_log_info(
+            'OnRspQryInvestorPosition:, pInvestorPosition: CThostFtdcInvestorPositionField, pRspInfo: CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool')
+        QA.QA_util_log_info(pInvestorPosition)
+        QA.QA_util_log_info(pRspInfo)
+        QA.QA_util_log_info(nRequestID)
+        QA.QA_util_log_info(bIsLast)
+
+    def OnRspQryTradingAccount(self, pTradingAccount: ctp.CThostFtdcTradingAccountField, pRspInfo: ctp.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
+        """[summary]
+        
+        Arguments:
+            pTradingAccount {ctp.CThostFtdcTradingAccountField} -- [description]
+            pRspInfo {ctp.CThostFtdcRspInfoField} -- [description]
+            nRequestID {int} -- [description]
+            bIsLast {bool} -- [description]
+        
+
+        BrokerID = '9999', 经纪公司代码
+        AccountID = '106184',投资者帐号 
+        PreMortgage = 0.0, 上次质押金额
+        PreCredit = 0.0, 上次信用额度
+        PreDeposit = 1442538.31, 上次存款额
+        PreBalance = 1443781.81, 上次结算准备金
+        PreMargin = 1243.5, 上次占用的保证金
+        InterestBase = 0.0, 利息基数
+        Interest = 0.0, 利息收入
+        Deposit = 0.0, 入金金额 
+        Withdraw = 0.0, 出金金额
+        FrozenMargin = 96536.0, 冻结的保证金
+        FrozenCash = 0.0, 冻结的资金
+        FrozenCommission = 107.4538, 冻结的手续费
+        CurrMargin = 1243.5, 当前保证金总额
+        CashIn = 0.0, 资金差额
+        Commission = 0.0, 手续费
+        CloseProfit = 0.0,平仓盈亏
+        PositionProfit = 30.0, 持仓盈亏
+        Balance = 1443811.81, 期货结算准备金
+        Available = 1345894.8562,可用资金
+        WithdrawQuota = 1076715.8849600002,可取资金
+        Reserve = 0.0,基本准备金
+        TradingDay = '20181116', 交易日
+        SettlementID = 1,结算编号
+        Credit = 0.0, 信用额度
+        Mortgage = 0.0, 质押金额
+        ExchangeMargin = 1243.5, 交易所保证金
+        DeliveryMargin = 0.0, 投资者交割保证金
+        ExchangeDeliveryMargin = 0.0,交易所交割保证金
+        ReserveBalance = 0.0, 保底期货结算准备金
+        CurrencyID = 'CNY', 币种代码
+        PreFundMortgageIn = 0.0, 上次货币质入金额
+        PreFundMortgageOut = 0.0, 上次货币质出金额
+        FundMortgageIn = 0.0,货币质入金额
+        FundMortgageOut = 0.0, 货币质出金额
+        FundMortgageAvailable = 0.0, 货币质押余额
+        MortgageableFund = 1076715.8849600002,可质押货币金额
+        SpecProductMargin = 0.0, 特殊产品占用保证金
+        SpecProductFrozenMargin = 0.0, 特殊产品冻结保证金
+        SpecProductCommission = 0.0, 特殊产品手续费
+        SpecProductFrozenCommission = 0.0,特殊产品冻结手续费
+        SpecProductPositionProfit = 0.0, 特殊产品持仓盈亏
+        SpecProductCloseProfit = 0.0, 特殊产品平仓盈亏
+        SpecProductPositionProfitByAlg = 0.0, 根据持仓盈亏算法计算的特殊产品持仓盈亏
+        SpecProductExchangeMargin = 0.0, 特殊产品交易所保证金
+        BizType = BizTypeType., 业务类型
+        FrozenSwap = 0.0,延时换汇冻结金额
+        RemainSwap = 0.0剩余换汇额度
+        """
+
+        QA.QA_util_log_info(
+            'OnRspQryTradingAccount:, pTradingAccount: CThostFtdcTradingAccountField, pRspInfo: CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool')
+        QA.QA_util_log_info(pTradingAccount)
+        QA.QA_util_log_info(pRspInfo)
+        QA.QA_util_log_info(nRequestID)
+        QA.QA_util_log_info(bIsLast)
+
+    def OnRspQryInvestor(self, pInvestor: ctp.CThostFtdcInvestorField, pRspInfo: ctp.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
+        QA.QA_util_log_info(
+            'OnRspQryInvestor:, pInvestor: CThostFtdcInvestorField, pRspInfo: CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool')
+        QA.QA_util_log_info(pInvestor)
+        QA.QA_util_log_info(pRspInfo)
+        QA.QA_util_log_info(nRequestID)
+        QA.QA_util_log_info(bIsLast)
 
     def OnRtnOrder(self, pOrder: ctp.CThostFtdcOrderField):
+        QA.QA_util_log_info('===ON_RTN_ORDE===')
         QA.QA_util_log_info(pOrder)
         QA.QA_util_log_info(pOrder.getOrderStatus())
+        QA.QA_util_log_info('===END_RTN_ORDE===')
         # if pOrder.getSessionID() == self.Session and pOrder.getOrderStatus() == ctp.OrderStatusType.NoTradeQueueing:
         #     QA.QA_util_log_info("撤单")
         #     self.t.ReqOrderAction(
@@ -205,7 +361,7 @@ class QA_ATBroker(QA_Broker):
         BidPrice5 = 1.7976931348623157e+308, BidVolume5 = 0, AskPrice5 = 1.7976931348623157e+308, AskVolume5 = 0, 
         AveragePrice = 38663.62041028492, ActionDay = '20181113'
         """
-        print(self.trading_code)
+        # QA.QA_util_log_info(self.trading_code)
         _thread.start_new_thread(self.tick_handle, (tick,))
         if not self.ordered:
             _thread.start_new_thread(self.Order, (f,))
@@ -298,8 +454,10 @@ class QA_ATBroker(QA_Broker):
         self.t.SubscribePrivateTopic(nResumeType=2)  # quick
         # self.t.SubscribePrivateTopic(nResumeType=2)
         self.t.Init()
-
-        # self.Qry()
+        self.t.ReqQryTradingAccount(self.broker, self.investor)
+        sleep(1.1)
+        #self.t.ReqQryInvestorPosition(self.broker, self.investor)
+        self.Qry()
         input()
         self.t.Release()
 
