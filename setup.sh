cde2ae348d3baac0ef4b074a63c57f37f0b263a2
yutiansut <yutiansut@qq.com>
2018-12-03 17:26:00 +0800

    #update

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 echo "QAATBROKER"
 cd ~ && git clone https://github.com/yutiansut/QA_AtBroker
-cd ~/.quantaxis && makedir bin && cp -r ~/QA_AtBroker/bin ~/.quantaxis/bin
+cp -r ~/QA_AtBroker/bin ~/.quantaxis
 
